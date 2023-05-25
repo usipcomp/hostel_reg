79671adf568f3fafec50376120cfa01eb95194fb
Aditya Mishra <adityavishu03@gmail.com>
2023-05-25 11:59:20 +0530

    fixed some more bug

--- src/Pages/StudentHome.js
+++ src/Pages/StudentHome.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Navbar from "../Components/Navbar";
 
 function StudentHome() {
   const user = useSelector((state) => state.user.currentUser);
 
   return (
-    <div className="w-full h-fit min-h-screen bg-[#edf6f9]">
+    <>
 
-      <div className="font-semibold m-10 text-2xl">
-        Welcome! {user.student.fullname}
-      </div>
-    </div>
+      {user?<div className="w-full h-fit min-h-screen bg-[#edf6f9]">
+
+        <div className="font-semibold m-10 text-2xl">
+          Welcome! {user.student.fullname}
+        </div>
+      </div>:<div>LOADING....</div>}
+    </>
   );
   // return <div>Welcome! {user.student.fullname}</div>;
 }
